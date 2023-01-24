391f1a6213be7c75646ab21d6781639803853263
Nidzjajal <110328065+Nidzjajal@users.noreply.github.com>
2023-01-24 22:32:13 +0530

    Add files via upload

---
+++ marksheet.js
@@ -0,0 +1,9 @@
+
+function sum()
+{
+	a=document.getElementById("sub1").value;
+	b=document.getElementById("sub2").value;	
+	c=document.getElementById("sub3").value;
+	total=a+b;
+	document.getElementById("answer").value=total;
+}
